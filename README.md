3db3e3d37a87ddf1a30973db05a967cc7807da6f
Artem Verbat <artem.verbat@gmail.com>
2016-10-25 15:10:41 +0300

    Added error handling to bundle scripts, views and styles. Added uglify to scripts task in production mode. Replace npm script for dev-mode from 'npm run serve' to 'npm start'.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 
 1. Run `npm install` to install dependencies.
 
-2. Run `gulp` or `npm run serve` to start the development server.  
+2. Run `gulp` or `npm start` to start the development server.  
    It should automatically open the client in your browser when ready.
 
 ### Build
--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,112 @@
 const gulp = require('gulp');
 const babelify = require('babelify');
 const browserify = require('browserify');
-const browserSync = require('browser-sync').create();
-const del = require('del');
-const gulpIf = require('gulp-if');
-const historyApiFallback = require('connect-history-api-fallback');
-const notify = require('gulp-notify');
-const plumber = require('gulp-plumber');
-const postcss = require('gulp-postcss');
-const pug = require('gulp-pug');
 const source = require('vinyl-source-stream');
-const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
 const stylus = require('gulp-stylus');
-const autoprefixer = require('autoprefixer');
-const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+const gulpIf = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const del = require('del');
+const gutil = require('gulp-util');
+const pug = require('gulp-pug');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 
 gulp.task('views', function buildHTML() {
-  return gulp.src('./src/views/**/*.pug')
-    .pipe(plumber({
-      errorHandler: notify.onError(function (err) {
-        return {
-          message: err.message
-        };
-      })
-    }))
-    .pipe(pug())
-    .pipe(gulp.dest('./public'));
+    return gulp.src('./src/views/**/*.pug')
+        .pipe(pug())
+        .on('error', function(error) {
+            gutil.log(gutil.colors.red('Error: ' + error.message));
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./public'));
 });
 
 gulp.task('styles', function () {
-  return gulp.src('./src/styles/app.styl')
-    .pipe(plumber({
-      errorHandler: notify.onError(function (err) {
-        return {
-          message: err.message
-        };
-      })
-    }))
-    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-    .pipe(stylus({
-      'include css': true
-    }))
-    .pipe(gulpIf(!isDevelopment, postcss([
-      autoprefixer({
-        browsers: ['> 5%', 'ff > 14']
-      })
-    ])))
-    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
-    .pipe(gulpIf(!isDevelopment, cleanCSS()))
-    .pipe(rename('style.css'))
-    .pipe(gulp.dest('./public'));
-}); 
+    return gulp.src('./src/styles/app.styl')
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+        .pipe(stylus({
+            'include css': true
+        })
+        .on('error', function(error) {
+            gutil.log(gutil.colors.red('Error: ' + error.message));
+            this.emit('end');
+        }))
+        .pipe(gulpIf(!isDevelopment, postcss([
+            autoprefixer({
+                browsers: ['> 5%', 'ff > 14']
+            })
+        ])))
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+        .pipe(gulpIf(!isDevelopment, cleanCSS()))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./public'))
+});
 
 gulp.task('scripts', function() {
-  return browserify({
-      entries: ['./src/scripts/app.js']
+    return browserify({
+        entries: './src/scripts/app.js',
+        debug: isDevelopment
     })
-    .transform(babelify, {presets: ["es2015"]})
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./public'))
+        .transform(babelify, {presets: ['es2015']})
+        .bundle()
+        .on('error', function(error) {
+            gutil.log(gutil.colors.red('Error: ' + error), '\n', error.codeFrame);
+            this.emit('end');
+        })
+        .pipe(source('bundle.js'))
+        .pipe(buffer()) // For `gulp-uglify`
+        .pipe(gulpIf(!isDevelopment, uglify()))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('copy:fonts', function () {
+    return gulp.src([
+        './node_modules/font-awesome/fonts/**/*.*'
+    ])
+        .pipe(gulp.dest('./public/fonts'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./src/views/**/*.pug', gulp.series('views'));
-  gulp.watch('./src/styles/**/*.styl', gulp.series('styles'));
-  gulp.watch('./src/**/*.js', gulp.series('scripts'));
+    gulp.watch('./src/views/**/*.pug', gulp.series('views'));
+    gulp.watch('./src/styles/**/*.styl', gulp.series('styles'));
+    gulp.watch('./src/**/*.js', gulp.series('scripts'));
 });
 
 gulp.task('serve', function () {
-  browserSync.init({
-    server: './public',
-    port: 8080,
-    middleware: [ historyApiFallback() ]
-  });
+    browserSync.init({
+        // proxy: 'print.loc',
+        // files: 'public/**/*.*',
+        server: './public',
+        port: 8080
+    });
 
-  browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
+    browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('clean', function () {
-  return del('./public')
+    return del('./public')
 });
 
 gulp.task('build', gulp.series(
-  'clean',
-  gulp.parallel(
-    'views',
-    'styles',
-    'scripts'
-)));
+    'clean',
+    gulp.parallel(
+        'views',
+        'styles',
+        'scripts',
+        'copy:fonts'
+    )));
 
 gulp.task('default', gulp.series(
-  'build',
-  gulp.parallel(
-    'watch',
-    'serve'
-)));
+    'build',
+    gulp.parallel(
+        'watch',
+        'serve'
+    )));
