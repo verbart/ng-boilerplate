c1af9a63ff82422dac0370be3f23edb0ba191c36
Artem Verbat <artem.verbat@gmail.com>
2016-12-27 00:34:44 +0200

    Added task for optimize images. A little refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,14 @@ const stylus = require('gulp-stylus');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
+const historyApiFallback = require('connect-history-api-fallback');
 const gulpIf = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const del = require('del');
 const gutil = require('gulp-util');
 const pug = require('gulp-pug');
+const tinypng = require('gulp-tinypng-nokey');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -62,30 +64,40 @@ gulp.task('scripts', function() {
             this.emit('end');
         })
         .pipe(source('bundle.js'))
-        .pipe(buffer()) // For `gulp-uglify`
+        .pipe(buffer())
         .pipe(gulpIf(!isDevelopment, uglify()))
         .pipe(gulp.dest('./public'));
 });
 
-gulp.task('copy:fonts', function () {
+gulp.task('fonts', function () {
     return gulp.src([
         './node_modules/font-awesome/fonts/**/*.*'
     ])
         .pipe(gulp.dest('./public/fonts'));
 });
 
+gulp.task('images', function () {
+    return gulp.src('./src/images/*.*')
+        .pipe(gulpIf(!isDevelopment, tinypng()))
+        .pipe(rename(function (path) {
+            path.dirname = '';
+        }))
+        .pipe(gulp.dest('./public/images'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('./src/views/**/*.pug', gulp.series('views'));
     gulp.watch('./src/styles/**/*.styl', gulp.series('styles'));
-    gulp.watch('./src/**/*.js', gulp.series('scripts'));
+    gulp.watch('./src/scripts/**/*.js', gulp.series('scripts'));
 });
 
 gulp.task('serve', function () {
     browserSync.init({
-        // proxy: 'print.loc',
+        // proxy: 'example.com',
         // files: 'public/**/*.*',
         server: './public',
-        port: 8080
+        port: 8080,
+        middleware: [ historyApiFallback() ]
     });
 
     browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
@@ -101,7 +113,8 @@ gulp.task('build', gulp.series(
         'views',
         'styles',
         'scripts',
-        'copy:fonts'
+        'fonts',
+        'images'
     )));
 
 gulp.task('default', gulp.series(
